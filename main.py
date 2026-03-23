018711edd7352e119569ccf5b55b055b936d3357
Alessandro Beretta <alex.beretta0923@outlook.com>
2026-03-23 16:50:43 +0100

    finalized react agent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +11,21 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilyResearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
 
-llm = ChatOpenAI()
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response with answer and sources"""
+
+    answer:str = Field(description="The agent answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources to generate the answer")
+
+
+llm = ChatOpenAI(model="gpt-5")
 tools = [TavilyResearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")

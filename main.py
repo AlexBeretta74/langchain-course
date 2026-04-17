360f29c68ac061849676d63cb9902487e4a1d346
Alessandro Beretta <alex.beretta0923@outlook.com>
2026-04-17 09:53:54 +0200

    nodes

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from dotenv import load_dotenv
-
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON="agent reason"
+ACT="act"
+LAST=-1
+
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow = StateGraph(MessagesState)
+flow.add_node(run_agent_reasoning, name=AGENT_REASON)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(tool_node, name=ACT)
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END:END,
+    ACT:ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 
 if __name__ == "__main__":

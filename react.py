b7f5b3955e8d2a51f6cca67ec0b37e1d88f0391a
Alessandro Beretta <alex.beretta0923@outlook.com>
2026-04-17 12:34:43 +0200

    final

--- react.py
+++ react.py
@@ -6,16 +6,13 @@ from langchain_tavily import TavilySearch
 load_dotenv()
 
 @tool
-def triple(num: float) -> float:
+def triple(num:float) -> float:
     """
-    param num: A number to be tripled
-    return: The tripled value of the input number
+    param num: a number to triple
+    returns: the triple of the input number
     """
     return float(num) * 3
 
 tools = [TavilySearch(max_results=1), triple]
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind(tools=tools)
-
-
-
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)

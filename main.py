8c2581af53b5ca34f7ee32a261ad89f543b8bea9
Alessandro Beretta <alex.beretta0923@outlook.com>
2026-04-17 16:15:43 +0200

    final

--- main.py
+++ main.py
@@ -19,10 +19,42 @@ def generation_node(state: MessageGraph):
 
 def reflection_node(state: MessageGraph):
     res = reflection_chain.invoke({"messages": state["messages"]})
-    return {"messages": [HumanMessage(content=res.content)]}   
+    return {"messages": [HumanMessage(content=res.content)]}
 
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue, path_map={REFLECT: REFLECT, END: END})
+builder.add_edge(REFLECT, GENERATE)
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
 
 
 if __name__ == "__main__":
     print("Hello, Reflection Agent!")
+    inputs = {
+        "messages": [
+            HumanMessage(
+                content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """
+            )
+        ]
+    }
+    response = graph.invoke(inputs)
+    print(response)
 

4e69c0ce9d427465b5ed0334a4ed71821e086ad7
Alessandro Beretta <alex.beretta0923@outlook.com>
2026-04-22 12:46:19 +0200

    Self-RAG

--- agentic-rag/graph/chains/tests/test_chains.py
+++ agentic-rag/graph/chains/tests/test_chains.py
@@ -6,6 +6,7 @@ load_dotenv()
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from graph.chains.generation import generation_chain
 from ingestion import retriever
+from graph.chains.hallucination_grader import GradeHallucinations, hallucination_grader
 
 def test_retrieval_grader_answer_yes() -> None:
     question = "agent memory"
@@ -34,3 +35,27 @@ def test_generation_chain() -> None:
     question = "agent memory"
     docs = retriever.invoke(question)
     generation = generation_chain.invoke({"question": question, "context": docs})
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"question": question, "context": docs})
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+
+    assert res.binary_score
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"question": question, "context": docs})
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": "in order to make pizza you need to first start with the dough"}
+    )
+
+    assert not res.binary_score
--- agentic-rag/graph/graph.py
+++ agentic-rag/graph/graph.py
@@ -3,6 +3,8 @@ from langgraph.graph import END, StateGraph
 from graph.consts import RETRIEVE, GRADE_DOCUMENTS, WEBSEARCH, GENERATE
 from graph.nodes import generate, grade_documents, retrieve, web_search
 from graph.state import GraphState
+from graph.chains.answer_grader import answer_grader
+from graph.chains.hallucination_grader import hallucination_grader
 
 load_dotenv()
 
@@ -18,6 +20,32 @@ def decide_to_generate(state):
         print("---DECISION: GENERATE---")
         return GENERATE
     
+def grade_generation_grounded_in_documents_and_question(state: GraphState) -> str:
+    print("---CHECK ALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+
+    if hallucination_grade := score.binary_score:
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke(
+            {"question": question, "generation": generation}
+        )
+        if answer_grade := score.binary_score:
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not_useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
 workflow = StateGraph(GraphState)
 
 workflow.add_node(RETRIEVE, retrieve)
@@ -31,6 +59,15 @@ workflow.add_conditional_edges(GRADE_DOCUMENTS, decide_to_generate, {
     WEBSEARCH: WEBSEARCH,
     GENERATE: GENERATE
 })
+workflow.add_conditional_edges(
+    GENERATE, 
+    grade_generation_grounded_in_documents_and_question, 
+    {
+    "not supported": WEBSEARCH,
+    "useful": END,
+    "not_useful": WEBSEARCH
+}
+)
 workflow.add_edge(WEBSEARCH, GENERATE)
 workflow.add_edge(GENERATE, END)
 
